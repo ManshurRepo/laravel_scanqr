df2efb4771c34074f18221a2dcf88f2525a711ef
azisdev <aziz.mcgr@gmail.com>
2024-07-08 14:35:52 +0700

    add API auth

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\ProdukController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,10 +20,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Register
+Route::post('/register', [AuthController::class, 'register']);
+//Login
+Route::post('/login', [AuthController::class, 'login']);
+//Logout
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 
 Route::get('/produk', [ProdukController::class, 'index']);
 Route::post('/produk', [ProdukController::class, 'store']);
 Route::put('/produk/{id}', [ProdukController::class, 'update']);
 Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
-
